1e4e35583387f362a6258181bc6b88b302b6fa77
KooSL <142590197+KooSL@users.noreply.github.com>
2023-08-18 16:49:49 +0545

    Add files via upload

---
+++ login.php
@@ -0,0 +1,12 @@
+<?php include 'includes/header.php'; ?>
+
+<form action="" class="login-form" method="POST">
+    <h1>Log In</h1>
+    <input type="text" placeholder="Username">
+    <input type="password" placeholder="Password">
+    <h4><a href="">Forget Password?</a></h4>
+    <input type="submit" value="Login">
+    <h5><a href="sign-up.php">Create new account</a></h5>
+</form>
+
+<?php include 'includes/footer.php'; ?>
---
+++ sign-up.php
@@ -0,0 +1,12 @@
+<?php include 'includes/header.php'; ?>
+
+<form action="" class="sign-up" method="POST">
+    <h1>Sign Up</h1>
+    <input type="text" placeholder="Full Name">
+    <input type="text" placeholder="Email">
+    <input type="password" placeholder="New Password">
+    <input type="submit" value="Sign Up">
+    <h5><a href="login.php">Already have a account</a></h5>
+</form>
+
+<?php include 'includes/footer.php'; ?>
