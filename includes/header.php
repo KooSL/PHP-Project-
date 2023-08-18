ed210271e4262bc64746ae83830f3b9ee36dd448
KooSL <142590197+KooSL@users.noreply.github.com>
2023-08-18 16:53:46 +0545

    Add files via upload

---
+++ includes/header.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title>News Portal</title>
+        <link rel="stylesheet" href="CSS/style.css">
+    </head>
+    <body>
+        <nav class="navbar">
+            <ul class="menu">
+                <li><a class="active" href="index.php">Home</a></li>
+                <li><a href="">News</a></li>
+                <li><a href="">Contact</a></li>
+                <li><a href="login.php">Login</a></li>
+            </ul>
+        </nav>
